ad582aab129884e84c5bd7cf037fdab5aeb1820b
Miguel E. Cabrera <mecg1994@gmail.com>
2025-04-21 08:34:31 -0600

    cambios

--- src/app/footer/componets/footer/footer.component.ts
+++ src/app/footer/componets/footer/footer.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  currentYear: number = 0;
+  showModal: boolean = false;
 
+  ngOnInit(): void {
+    this.currentYear = new Date().getFullYear();
+  }
+
+  copyEmail(): void {
+    navigator.clipboard.writeText('mecg1994@gmail.com');
+    // Opcional: puedes agregar una notificación o mensaje aquí
+  }
 }
